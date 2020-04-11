a6fc727ade8f253409f45c3331a762ecbdc344c0
Louis-Marie Michelin <lmichelin@outlook.fr>
2020-04-11 21:07:15 +0200

    ✅ use cypress instead of nightwatch

---
+++ cypress/plugins/index.js
@@ -0,0 +1,10 @@
+/// <reference types="cypress" />
+
+const path = require("path")
+
+module.exports = on => {
+  on("before:browser:launch", (browser, launchOptions) => {
+    launchOptions.extensions.push(path.join(__dirname, `../../dist/${browser.name}`))
+    return launchOptions
+  })
+}
