c48727c875a99e32c328ac150c253a549076a4cf
Louis-Marie Michelin <lmichelin@outlook.fr>
2021-05-08 00:09:48 +0200

    tests: ignore random error on Firefox

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,1 +1,10 @@
 import "./commands"
+
+Cypress.on("uncaught:exception", (err, runnable, promise) => {
+  // Ignore this unhandled promise rejection error happening randomly on Firefox
+  if (promise && err.message.includes("NetworkError when attempting to fetch resource.")) {
+    return false
+  }
+  // we still want to ensure there are no other unexpected
+  // errors, so we let them fail the test
+})
