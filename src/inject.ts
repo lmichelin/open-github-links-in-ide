62d9294a7241b7a205db2a2e6145f40abbafce35
Victor Igualada <victor.igualada@kevin.eu>
2023-04-20 16:21:32 +0200

    Add Gitlab support

--- src/inject.ts
+++ src/inject.ts
@@ -10,6 +10,11 @@ const run = async () => {
     if (OPTIONS.showDebugMessages) console.log.apply(null, ["[OPEN-IN-IDE EXTENSION]", ...args])
   }
 
+  const HOSTS: { [h: string]: string } = {
+    "github.com": "github",
+    "gitlab.com": "gitlab",
+  }
+
   const EDITOR_OPENERS: {
     [e in Editor]: (repo: string, file: string, line?: string) => string
   } = {
@@ -83,6 +88,18 @@ const run = async () => {
     },
   }
 
+  const addEditorIcons = () => {
+    switch (HOSTS[location.host]) {
+      case "github":
+        addEditorIconsGithub()
+        break
+      case "gitlab":
+        addEditorIconsGitlab()
+        break
+      default:
+    }
+  }
+
   const generateIconElement = (repo: string, file: string, lineNumber?: string | null) => {
     const editorIconSpanElement = document.createElement("span")
     const filename = file.split("/").pop() as string
@@ -90,6 +107,7 @@ const run = async () => {
     if (lineNumber) iconTitle = `${iconTitle} at line ${lineNumber}`
     editorIconSpanElement.title = iconTitle
     editorIconSpanElement.classList.add("open-in-ide-icon")
+    editorIconSpanElement.setAttribute("data-path", file) // Adds this argument to ease comparison on Gitlab lazy reload
 
     const editorIconImgElement = document.createElement("img")
     editorIconImgElement.src = chrome.runtime.getURL(EDITORS[OPTIONS.defaultIde].getIcon(32))
@@ -103,9 +121,10 @@ const run = async () => {
     return editorIconSpanElement
   }
 
-  const filePathRegExp = /.+\/([^/]+)\/(blob|tree)\/[^/]+\/(.*)/
+  const filePathRegExpGithub = /.+\/([^/]+)\/(blob|tree)\/[^/]+\/(.*)/
+  const filePathRegExpGitlab = /\/([^\/]+)\/-\/(?:blob|tree)(?:\/[^\/]+)?\/(.+)$/
 
-  const addEditorIcons = () => {
+  const addEditorIconsGithub = () => {
     debug("Adding editor icons")
 
     let addedIconsCounter = 0
@@ -124,9 +143,9 @@ const run = async () => {
         if (fileElement.parentNode?.querySelector(".open-in-ide-icon")) return
 
         const fileUrl = fileElement.querySelector("a")?.getAttribute("href")
-        if (!fileUrl || !filePathRegExp.test(fileUrl)) return
+        if (!fileUrl || !filePathRegExpGithub.test(fileUrl)) return
 
-        const pathInfo = filePathRegExp.exec(fileUrl)
+        const pathInfo = filePathRegExpGithub.exec(fileUrl)
         const repo = pathInfo?.[1]
         const file = pathInfo?.[3]
         if (!repo || !file) return
@@ -224,11 +243,142 @@ const run = async () => {
     debug(`Added ${addedIconsCounter} new editor icons`)
   }
 
+  // Gitlab lazy loads the file blocks, so we need to remove the old icon if it's not the same file
+  const reloadIconIfMismatchesWithFile = (file: string, icon: Element | null) => {
+    if (icon && icon.getAttribute("data-path") !== file) {
+      debug("Removed mismatched icon for file: " + file)
+      icon.remove()
+    }
+  }
+
+  const addEditorIconsGitlab = () => {
+    debug("Adding editor icons")
+
+    let addedIconsCounter = 0
+
+    // -------------------------------
+    // repository content (files list)
+    // -------------------------------
+
+    if (OPTIONS.showIconInFileTree) {
+      const files = document.querySelectorAll(".tree-item-file-name")
+      files.forEach(fileData => {
+        const fileElement = fileData.querySelector("a")
+        if (!fileElement) return
+
+        // don't add a new icon if icon already exists
+        if (fileElement.parentNode?.querySelector(".open-in-ide-icon")) return
+        const fileUrl = fileElement.getAttribute("href")
+        if (!fileUrl || !filePathRegExpGitlab.test(fileUrl)) return
+
+        const pathInfo = filePathRegExpGitlab.exec(fileUrl)
+        const repo = pathInfo?.[1]
+        const file = pathInfo?.[2]
+
+        if (!repo || !file) return
+        const editorIconElement = generateIconElement(repo, file)
+        editorIconElement.classList.add("open-in-ide-icon-file-explorer")
+
+        fileElement.parentNode?.insertBefore(editorIconElement, fileElement.nextSibling)
+        addedIconsCounter++
+      })
+    }
+
+    // --------------------------------------------
+    // file links (files changed view & discussions)
+    // --------------------------------------------
+
+    if (OPTIONS.showIconOnFileBlockHeaders || OPTIONS.showIconOnLineNumbers) {
+      const inFilesChangedView = document
+        .querySelector(".merge-request-tabs")
+        ?.querySelector(".active")
+        ?.querySelector("a")
+        ?.innerText.includes("Changes")
+
+      // select file blocks
+      const primaryLinks = document.querySelectorAll(".file-header-content")
+
+      const repo = window.location.href.split("/-")[0]?.split("/").pop()
+
+      primaryLinks.forEach(linkElement => {
+        const file = linkElement.querySelector(".file-title-name")?.getAttribute("title")?.trim().split(" ")[0]
+
+        // no file or repo found
+        if (!repo || !file) return
+
+        let lineNumberForFileBlock
+
+        const fileElement = linkElement.closest(inFilesChangedView ? ".diff-file" : ".js-discussion-container")
+
+        if (fileElement) {
+          if (!inFilesChangedView) {
+            // in discussion
+            const lineNumberNodes = fileElement.querySelectorAll(".diff-line-num.new_line")
+
+            if (lineNumberNodes.length === 0) return // length can be equal to zero in case of resolved comment for example
+
+            // get last line number
+            lineNumberForFileBlock = lineNumberNodes[lineNumberNodes.length - 1].innerHTML
+          } else {
+            const firstLineNumberNode = fileElement.querySelector("a[data-linenumber]")
+            // get first line number
+            lineNumberForFileBlock = firstLineNumberNode?.getAttribute("data-linenumber")
+          }
+        } else {
+          // no line number available
+        }
+
+        if (OPTIONS.showIconOnFileBlockHeaders) {
+          const existingIcon = linkElement.querySelector(".open-in-ide-icon")
+
+          reloadIconIfMismatchesWithFile(file, existingIcon)
+
+          if (existingIcon) return // don't add a new icon if icon already exists and matches the file
+
+          const editorIconElement = generateIconElement(repo, file, lineNumberForFileBlock)
+
+          linkElement.insertBefore(editorIconElement, null)
+          addedIconsCounter++
+        }
+
+        if (OPTIONS.showIconOnLineNumbers && fileElement) {
+          const clickableLineNumbersNodes = fileElement.querySelectorAll("a[data-linenumber]")
+
+          clickableLineNumbersNodes.forEach(lineNumberNode => {
+            const existingIcon = lineNumberNode.querySelector(".open-in-ide-icon")
+
+            reloadIconIfMismatchesWithFile(file, existingIcon)
+
+            if (existingIcon) return // don't add a new icon if icon already exists and matches the file
+
+            const lineNumber = lineNumberNode.getAttribute("data-linenumber")
+            const editorIconElement = generateIconElement(repo, file, lineNumber)
+
+            lineNumberNode.classList.add("js-open-in-ide-icon-added")
+            lineNumberNode.appendChild(editorIconElement)
+            addedIconsCounter++
+          })
+        }
+      })
+    }
+
+    debug(`Added ${addedIconsCounter} new editor icons`)
+  }
+
   // observe content changes
   const observeChanges = () => {
     debug("Observing page changes")
+    let content
 
-    const content = document.querySelector(".repository-content")
+    switch (HOSTS[location.host]) {
+      case "github":
+        content = document.querySelector(".repository-content")
+        break
+      case "gitlab":
+        content = document.querySelector("#content-body")
+        break
+      default:
+    }
 
     if (content)
       pageChangeObserver.observe(content, {
