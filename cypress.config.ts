44dce6db504acd6dba409df2b977abc18f3dfdc7
Louis-Marie Michelin <lmichelin@outlook.fr>
2023-09-23 10:08:58 +0200

    lint cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from "cypress"
 
 export default defineConfig({
   video: false,
@@ -6,8 +6,9 @@ export default defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      // eslint-disable-next-line
+      return require("./cypress/plugins/index.js")(on, config)
     },
-    baseUrl: 'https://github.com',
+    baseUrl: "https://github.com",
   },
 })
